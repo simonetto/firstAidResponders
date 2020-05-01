dc0d1022ed9dfdcdfa554fe8b35013e7a6cb9ee0
Renzo Simonetto <renzo.bertero@tomra.com>
2020-05-01 11:35:44 +0100

    Improve styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default () => (
     <div className="application">
         <Helmet>
             <meta charSet="utf-8" />
-            <title>First Aid Responders</title>
+            <title>Bray Cardiac First Responders</title>
             <link rel="canonical" href="http://braycfr.ie" />
             <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
         </Helmet>
